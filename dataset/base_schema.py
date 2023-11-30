3e0d50c5e8050565731e6c00b3cea03b98ee90ee
leo.feng <leo.feng@appier.com>
2023-11-30 16:13:00 +0800

    add schema

---
+++ dataset/base_schema.py
@@ -0,0 +1,23 @@
+"""
+should define the available feature name
+"""
+
+class Schema:
+    
+    @staticmethod
+    def check_required(feature):
+        """check for required features"""
+        required_features_example = (
+            'feature_A',
+            'feature_B'
+        )
+    
+    @staticmethod
+    def check_optional(feature):
+        """check for optional features"""
+        optional_features_example = (
+            'feature_A',
+            'feature_B'
+        )
+        
+        
